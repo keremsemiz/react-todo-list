c03d3dfdfa0324dc7638b4465d1025dcaeb6fb8e
Kerem Semiz <kerox350@gmail.com>
2024-07-03 17:05:23 +0300

    Project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+export default function App (){
+  const [newItem, setNewItem] = useState("")
+  return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form className='new-item-form'>
+        <div className="form-row">
+          <label htmlFor='item'>New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className='btn'>Add Task</button>
+      </form>
+      <h1 className='header'>To-Do List</h1>
+      <ul className='list'>
+        <li>
+          <label>
+            <input type="checkbox" name="" id="" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" name="" id="" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" name="" id="" />
+            Item 3
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
     </>
   )
 }
-
-export default App
