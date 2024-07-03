2469e21f22d5c25032182b3aba4622c61c0131d5
Kerem Semiz <kerox350@gmail.com>
2024-07-03 22:41:15 +0200

    Learnt about localstorage, implemented it and added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 export default function App (){
   const [newItem, setNewItem] = useState("")
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue === null) return []
+    
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
 
   function handleSubmit(e){
     e.preventDefault()
@@ -37,31 +46,34 @@ export default function App (){
   }
   return(
     <>
-      <form onSubmit={handleSubmit} className='new-item-form'>
-        <div className="form-row">
-          <label htmlFor='item'>New Item</label>
-          <input value={newItem}
-            type="text"
-            id="item"
-            onChange={e => setNewItem(e.target.value)} />
-        </div>
-        <button className='btn'>Add Task</button>
-      </form>
-      <h1 className='header'>To-Do List</h1>
-      <ul className='list'>
-        {todos.length === 0 && "No current To-Dos"}
-        {todos.map(todo => {
-          return (
-            <li key={todo.id} >
-            <label>
-              <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
-              {todo.title}
-            </label>
-            <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
-          </li>
-          )
-        })}
-      </ul>
-    </>
+    <header className='navbar'>
+      <h2 className='navbar-title'>To-Do App</h2>
+    </header>
+    <form onSubmit={handleSubmit} className='new-item-form'>
+      <div className="form-row">
+        <label htmlFor='item'>New Item</label>
+        <input value={newItem}
+          type="text"
+          id="item"
+          onChange={e => setNewItem(e.target.value)} />
+      </div>
+      <button className='btn'>Add Task</button>
+    </form>
+    <h1 className='header'>To-Do List</h1>
+    <ul className='list'>
+      {todos.length === 0 && "No current To-Dos"}
+      {todos.map(todo => {
+        return (
+          <li key={todo.id} >
+          <label>
+            <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
+            {todo.title}
+          </label>
+          <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
+        </li>
+        )
+      })}
+    </ul>
+  </>
   )
 }
