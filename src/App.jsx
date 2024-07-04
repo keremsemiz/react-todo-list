7e62d65e40f659a500eee6443bd9d8cadfe21352
Kerem Semiz <kerox350@gmail.com>
2024-07-04 14:28:56 +0300

    Finalize project!

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,30 @@ import './App.css'
 
 export default function App() {
   const [newItem, setNewItem] = useState("")
+  const [isEditing, setIsEditing] = useState(false)
+  const [currentTodo, setCurrentTodo] = useState(null)
   const [todos, setTodos] = useState(() => {
     const localValue = localStorage.getItem("ITEMS")
     if (localValue === null) return []
     return JSON.parse(localValue)
   })
-  const [time, setTime] = useState(new Date().toLocaleTimeString())
-  const [isEditing, setIsEditing] = useState(false)
-  const [currentTodo, setCurrentTodo] = useState(null)
+  const [completedTodos, setCompletedTodos] = useState(() => {
+    const localValue = localStorage.getItem("COMPLETEDITEMS")
+    if (localValue === null) return []
+    return JSON.parse(localValue)
+  })
 
   useEffect(() => {
     localStorage.setItem("ITEMS", JSON.stringify(todos))
   }, [todos])
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date().toLocaleTimeString())
-    }, 1000)
-    return () => clearInterval(interval)
-  }, [])
+    localStorage.setItem("COMPLETEDITEMS", JSON.stringify(completedTodos))
+  }, [completedTodos])
 
-  function handleSubmit(e) {
+  function handleSubmit(e){
     e.preventDefault()
-    if (isEditing) {
+    if (isEditing){
       setTodos(currentTodos => currentTodos.map(todo => todo.id === currentTodo.id ? { ...todo, title: newItem } : todo))
       setIsEditing(false)
       setCurrentTodo(null)
@@ -34,37 +35,49 @@ export default function App() {
         ...currentTodos, { id: crypto.randomUUID(), title: newItem, completed: false },
       ])
     }
+
     setNewItem("")
   }
 
-  function toggleTodo(id, completed) {
-    setTodos(currentTodos => {
-      return currentTodos.map(todo => {
-        if (todo.id === id) {
-          return { ...todo, completed }
-        }
-        return todo
-      })
-    })
-  }
-
-  function deleteTodo(id) {
-    setTodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id !== id)
-    })
-  }
-
-  function editTodo(todo) {
+  function editTodo(todo){
     setIsEditing(true)
     setCurrentTodo(todo)
     setNewItem(todo.title)
   }
 
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      const todo = currentTodos.find(todo => todo.id === id)
+      if (todo) {
+        todo.completed = completed
+        if (completed) {
+          setCompletedTodos([...completedTodos, todo])
+          return currentTodos.filter(todo => todo.id !== id)
+        }
+      }
+      return currentTodos
+    })
+
+    setCompletedTodos(currentCompletedTodos => {
+      const todo = currentCompletedTodos.find(todo => todo.id === id)
+      if (todo) {
+        todo.completed = completed
+        if (!completed) {
+          setTodos([...todos, todo])
+          return currentCompletedTodos.filter(todo => todo.id !== id)
+        }
+      }
+      return currentCompletedTodos
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos(currentTodos => currentTodos.filter(todo => todo.id !== id))
+    setCompletedTodos(currentCompletedTodos => currentCompletedTodos.filter(todo => todo.id !== id))
+  }
+
   return (
     <>
-      <div className="navbar">
-        <h1 className="navbar-title">Current Time: {time}</h1>
-      </div>
       <form onSubmit={handleSubmit} className='new-item-form'>
         <div className="form-row">
           <label htmlFor='item'>{isEditing ? 'Edit Item' : 'New Item'}</label>
@@ -74,7 +87,7 @@ export default function App() {
             id="item"
             onChange={e => setNewItem(e.target.value)} />
         </div>
-        <button className='btn'>{isEditing ? 'Update Task' : 'Add Task'}</button>
+        <button className='btn'>{isEditing ? 'Update Task' : 'Add task'}</button>
       </form>
       <h1 className='header'>To-Do List</h1>
       <ul className='list'>
@@ -87,6 +100,21 @@ export default function App() {
                 {todo.title}
               </label>
               <button onClick={() => editTodo(todo)} className="btn btn-safety">Edit</button>
+              <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
+            </li>
+          )
+        })}
+      </ul>
+      <h1 className='header'>Already Completed</h1>
+      <ul className='list'>
+        {completedTodos.length === 0 && "No completed Todos"}
+        {completedTodos.map(todo => {
+          return (
+            <li key={todo.id} >
+              <label>
+                <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
+                {todo.title}
+              </label>
               <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
             </li>
           )
